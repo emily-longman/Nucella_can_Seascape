d6db2a2eb2c956a9ef13b508ac33814c3f4feda8
Emily Longman <Emily.Longman@uvm.edu>
2025-06-26 10:07:28 -0400

    Update 03_graph_glms.R

--- src/02_GEA/02_glms/03_graph_glms.R
+++ src/02_GEA/02_glms/03_graph_glms.R
@@ -17,7 +17,7 @@ setwd(root_path)
 # ================================================================================== #
 
 # Load packages
-# install.packages(c('data.table', 'tidyverse', 'plyr, 'foreach', 'ggplot2'))
+install.packages(c('data.table', 'tidyverse', 'plyr', 'foreach', 'ggplot2'))
 library(data.table)
 library(tidyverse)
 library(plyr)
@@ -29,56 +29,15 @@ library(ggplot2)
 # Load data
 
 # Create list of file names
-file_names = as.list(dir(path = 'data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_1/', pattern = "GLM_100perm_Bio-Oracle_*"))
-file_names = lapply(file_names, function(x) paste0('data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_1/', x))
-
-out = lapply(file_names, function(x){
-  env = new.env()
-  nm = load(x, envir = env)[1]
-  objname = gsub(pattern = 'data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_1/', replacement = '', x = x, fixed = T)
-  objname = gsub(pattern = 'GLM_100perm_Bio-Oracle_|.RData', replacement = '', x = objname)
-  assign(objname, env[[nm]], envir = .GlobalEnv)
-  0 # succeeded
-} )
-
-
-
-
-load("data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_1/GLM_100perm_Bio-Oracle_chunk_1_chr_Backbone_17064_start_1568_stop_28650.Rdata")
-
-load("data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_1/GLM_100perm_Bio-Oracle_chunk_1_chr_Backbone_17065_start_678_stop_50678.Rdata")
-
-#### Struggling to load each and merge them together
-
-my_files <- list.files(path = "data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_chunk_1", pattern = "GLM_100perm_Bio-Oracle_*", full.names = T)
-
-temp <- new.env()
-list <- lapply(my_files, load, temp)
-
-all_data <- as.list(temp)
-rm(temp) 
-
-glm.model.output.all <- bind_rows(all_data)
-
-
-
-str(glm.model.output.all)
-
-
-
-
-
-
-#load important libraries  
-library(tidyverse)  
-library(foreach)
+file_names = as.list(dir(path = 'data/processed/GEA/glms/glms_window_analysis/', pattern = "GLM_100perm_Bio-Oracle_chunk_*"))
+file_names = lapply(file_names, function(x) paste0('data/processed/GEA/glms/glms_window_analysis/', x))
   
-#use a loading loop with system to read all the files  
-command = paste("ls | grep 'output2' " )  
-files_o = system(command, intern = TRUE )  
-  
+# Read all the files 
 collated.files =  
-foreach(i=files_o, .combine="rbind")%do%{  
-message(i)  
-o = get(load(i))  #add mutate(...) to add new column with RData object name
+foreach(i=file_names, .combine="rbind")%do%{  
+message(i) 
+chunk = i
+o = get(load(i))
+o %>% mutate(chunk = file.name) %>% mutate(chunk = str_remove(chunk, pattern = "data/processed/GEA/glms/glms_window_analysis/GLM_100perm_Bio-Oracle_"))
 } 
+
