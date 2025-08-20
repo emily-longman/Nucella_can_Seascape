6aa403d706aec418066aa7294f207b83db3c89cb
Emily Longman <Emily.Longman@uvm.edu>
2025-08-20 16:42:29 -0400

    Update 02_glms_bio-oracle_redos_part1.sh

--- src/02_GEA/02_glms/01_run_glms/02_glms_bio-oracle_redos_part1.sh
+++ src/02_GEA/02_glms/01_run_glms/02_glms_bio-oracle_redos_part1.sh
@@ -17,7 +17,7 @@
 #SBATCH --time=2-00:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=100G 
+#SBATCH --mem=200G 
 
 # Request CPU
 #SBATCH --cpus-per-task=5
