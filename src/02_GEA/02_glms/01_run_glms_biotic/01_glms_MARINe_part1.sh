51b872b804cdf4905bd60c4f66f4a8388f472108
Emily Longman <Emily.Longman@uvm.edu>
2025-09-21 22:07:40 -0400

    Update glms

--- src/02_GEA/02_glms/01_run_glms_biotic/01_glms_MARINe_part1.sh
+++ src/02_GEA/02_glms/01_run_glms_biotic/01_glms_MARINe_part1.sh
@@ -14,7 +14,7 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=10:00:00 #5:00:00 for real
+#SBATCH --time=20:00:00 #5:00:00 for real
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=30G 
@@ -23,7 +23,7 @@
 #SBATCH --cpus-per-task=5
 
 # Submit job array
-#SBATCH --array=1-500%50
+#SBATCH --array=77 #1-500%50
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
