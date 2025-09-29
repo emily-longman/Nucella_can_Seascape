291563c5d1d9dd8498aaab78b95c3fa16956aedf
Emily Longman <Emily.Longman@uvm.edu>
2025-09-29 06:36:32 -0400

    Update 01_glms_MARINe_perm26:50.R

--- src/02_GEA/02_glms/01_run_glms_biotic/01_glms_MARINe_perm26:50.R
+++ src/02_GEA/02_glms/01_run_glms_biotic/01_glms_MARINe_perm26:50.R
@@ -243,7 +243,7 @@ glm.model.output =
 # Generate folders and save output
 
 # Folder name 
-folder_name <- paste("data/processed/GEA/glms/glms_chunk_analysis_marine/perm_1_25")
+folder_name <- paste("data/processed/GEA/glms/glms_chunk_analysis_marine/perm_26_50")
 if (!dir.exists(folder_name)) {dir.create(folder_name)}
 
 # Save file for chunk w
