17ed0dffc473b21d65adb8118d55c366c80e5fec
Emily Longman <Emily.Longman@uvm.edu>
2025-11-07 09:19:41 -0500

    Update 02_merge_glms.sh

--- src/02_GEA/02_glms/01_run_glms_biotic/02_merge_glms.sh
+++ src/02_GEA/02_glms/01_run_glms_biotic/02_merge_glms.sh
@@ -50,8 +50,8 @@ SCRIPT_FOLDER=$WORKING_FOLDER/src/02_GEA/02_glms/01_run_glms_biotic
 
 #Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_real.R
 #Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_perm_1:25.R
-Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_perm_26:50.R
-#Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_perm_51:75.R
+#Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_perm_26:50.R
+Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_perm_51:75.R
 #Rscript --vanilla $SCRIPT_FOLDER/02_merge_glms_perm_76:100.R
 # The --vanilla option prevents restoring or saving workspaces
 
