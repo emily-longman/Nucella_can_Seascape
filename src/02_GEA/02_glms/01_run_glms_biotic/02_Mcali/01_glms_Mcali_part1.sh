47da2994f6d425dee067545a1621c8543443160a
Emily Longman <Emily.Longman@uvm.edu>
2025-12-21 16:10:37 -0600

    GLM M. cali

--- src/02_GEA/02_glms/01_run_glms_biotic/02_Mcali/01_glms_Mcali_part1.sh
+++ src/02_GEA/02_glms/01_run_glms_biotic/02_Mcali/01_glms_Mcali_part1.sh
@@ -5,7 +5,7 @@
 # Request cluster resources ----------------------------------------------------
 
 # Name this job
-#SBATCH --job-name=glms_marine_part1_perm1_25
+#SBATCH --job-name=glms_marine_part1_perm26_50
 
 # Specify partition
 #SBATCH --partition=general
@@ -98,8 +98,8 @@ fi
 # Run R script
 
 #Rscript $SCRIPT_FOLDER/01_glms_Mcali_real.R "${SLURM_ARRAY_TASK_ID}"
-Rscript $SCRIPT_FOLDER/01_glms_Mcali_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
-#Rscript $SCRIPT_FOLDER/01_glms_Mcali_perm26:50.R "${SLURM_ARRAY_TASK_ID}"
+#Rscript $SCRIPT_FOLDER/01_glms_Mcali_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
+Rscript $SCRIPT_FOLDER/01_glms_Mcali_perm26:50.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_Mcali_perm51:75.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_Mcali_perm76:100.R "${SLURM_ARRAY_TASK_ID}"
 
