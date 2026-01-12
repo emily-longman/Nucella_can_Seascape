4f0a84645f6eba01f6c01938bedf9cacbe0a883e
Emily Longman <Emily.Longman@uvm.edu>
2026-01-12 08:27:03 -0500

    Update 01_endemism.sh

--- src/06_endemism/01_endemism.sh
+++ src/06_endemism/01_endemism.sh
@@ -20,7 +20,7 @@
 #SBATCH --mem=50G 
 
 # Submit job array
-#SBATCH --array=1-10 #500%50
+#SBATCH --array=11-500%75 #1-10
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
