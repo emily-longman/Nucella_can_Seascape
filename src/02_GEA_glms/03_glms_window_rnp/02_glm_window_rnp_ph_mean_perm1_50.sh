57610fd38adab285fd08f08419d583664cf55e64
Emily Longman <Emily.Longman@uvm.edu>
2026-01-13 20:26:02 -0500

    Update 02_glm_window_rnp_ph_mean_perm1_50.sh

--- src/02_GEA_glms/03_glms_window_rnp/02_glm_window_rnp_ph_mean_perm1_50.sh
+++ src/02_GEA_glms/03_glms_window_rnp/02_glm_window_rnp_ph_mean_perm1_50.sh
@@ -14,13 +14,13 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=01:00:00 
+#SBATCH --time=00:40:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=300G 
+#SBATCH --mem=200G 
 
 # Submit job array
-#SBATCH --array=1-5 #1-995%75
+#SBATCH --array=1-995%75
 
 # Request CPU
 #SBATCH --cpus-per-task=10
