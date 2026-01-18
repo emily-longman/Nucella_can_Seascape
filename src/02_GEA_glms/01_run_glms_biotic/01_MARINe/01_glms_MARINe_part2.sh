d4358d8d0c0ae2082db033f86d3fc35cf339737a
Emily Longman <Emily.Longman@uvm.edu>
2026-01-17 22:18:17 -0500

    Update 01_glms_MARINe_part2.sh

--- src/02_GEA_glms/01_run_glms_biotic/01_MARINe/01_glms_MARINe_part2.sh
+++ src/02_GEA_glms/01_run_glms_biotic/01_MARINe/01_glms_MARINe_part2.sh
@@ -14,16 +14,16 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=15:00:00 #30:00:00 #5:00:00 for most real
+#SBATCH --time=30:00:00 #5:00:00 for most real
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=30G 
 
 # Request CPU
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=7
 
 # Submit job array
-#SBATCH --array=728,865,866,867,869,871,873,874,880,923,925,927,928,930,954,955,956,984 #501-996%75
+#SBATCH --array=501-996%75
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
@@ -97,8 +97,8 @@ fi
 
 # Run R script
 
-Rscript $SCRIPT_FOLDER/01_glms_MARINe_real.R "${SLURM_ARRAY_TASK_ID}"
-#Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
+#Rscript $SCRIPT_FOLDER/01_glms_MARINe_real.R "${SLURM_ARRAY_TASK_ID}"
+Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm26:50.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm51:75.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm76:100.R "${SLURM_ARRAY_TASK_ID}"
