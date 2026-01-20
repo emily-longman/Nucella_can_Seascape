ac17e794e35d6479de83d7d8203c1ad5e0b3fea2
Emily Longman <Emily.Longman@uvm.edu>
2026-01-20 12:00:41 -0500

    Update baypass thresholds

--- src/03_baypass/06_graph_baypass_topvars.R
+++ src/03_baypass/06_graph_baypass_topvars.R
@@ -42,11 +42,12 @@ snp.meta <- read.table("data/processed/baypass/input_files/snpdet", header=F)
 # Re-name snp metadata
 colnames(snp.meta) <- c("chr", "pos", "allele1", "allele2")
 
-# Load POD threshold
+# ================================================================================== #
+# ================================================================================== #
+
+# Load POD thresholds
 load("data/processed/baypass/abiotic/ph_mean_POD_thr.Rdata")
 
-# ================================================================================== #
- 
 # Read in Baypass pH mean files
 
 # Load BF output for 5 replicate Baypass runs
@@ -68,10 +69,13 @@ bf.ph.mean.sum <- bf.ph.mean %>% group_by(chr, pos, allele1, allele2, MRK) %>%
     reframe(bf_db.mean = mean(bf_db), bf_db.median = median(bf_db),
             bf_db.var=var(bf_db), eBPis.mean=mean(eBPis), eBPis.median=median(eBPis), eBPis.var=var(eBPis))
 
+# Save
+save(bf.ph.mean.sum, file="data/processed/baypass/abiotic/bf.ph.mean.sum.Rdata")
+
 # ================================================================================== #
 
-# Graph BF with POD threshold
-pdf("output/figures/baypass/baypass_BF_ph_mean_rep.means.pdf", width = 12, height = 8)
+# Graph BF with 0.001 POD threshold
+pdf("output/figures/baypass/baypass_BF_ph_mean_repmeans.pdf", width = 12, height = 8)
 ggplot(bf.ph.mean.sum, aes(y=bf_db.mean, x=chr)) + 
   labs(x = "Position", y = "BF (in dB)") +
   geom_point(alpha=0.6) + 
@@ -81,8 +85,8 @@ ggplot(bf.ph.mean.sum, aes(y=bf_db.mean, x=chr)) +
         axis.text.y = element_text(size = 12))
 dev.off()
 
-# Graph BF with POD threshold - without BF < 0
-pdf("output/figures/baypass/baypass_BF_ph_mean_5rep_posBF.pdf", width = 12, height = 8)
+# Graph BF with 0.001 POD threshold - only BF > 0
+pdf("output/figures/baypass/baypass_BF_ph_mean_repmeans_posBF.pdf", width = 12, height = 8)
 ggplot(bf.ph.mean.sum[which(bf.ph.mean.sum$bf_db.mean>0),], aes(y=bf_db.mean, x=chr)) + 
   labs(x = "Position", y = "BF (in dB)") +
   geom_point(alpha=0.6) + 
@@ -96,8 +100,70 @@ dev.off()
 
 # Identify patterns in bayes factors
 
-# pH mean - 1,673 SNPs with BF > threshold
+# pH mean - 1,828 SNPs with BF > threshold
 bf.ph.mean.sum.outliers <- bf.ph.mean.sum[which(bf.ph.mean.sum$bf_db.mean > bf.POD.thr$bf_db.mean[which(bf.POD.thr$thr==0.999)]),]
 bf.ph.mean.sum.outliers <- bf.ph.mean.sum.outliers %>% mutate(SNP_id = paste(chr, pos, sep = "_"))
 write.csv(bf.ph.mean.sum.outliers, "data/processed/baypass/bf.ph.mean.sum.outliers.csv", row.names = F, quote = F)
 
+# ================================================================================== #
+# ================================================================================== #
+
+# Load POD thresholds
+load("data/processed/baypass/biotic/Mtross_mean_POD_thr.Rdata")
+
+# Read in Baypass M tross mean files
+
+# Load BF output for 5 replicate Baypass runs
+bf.Mtross.mean <- foreach(i=1:5, .combine = rbind)%do%{
+    message(i)
+    tmp <- fread(paste("data/processed/baypass/biotic/Mtross_mean/NC_biotic_Mtross_mean_run", i, "_summary_betai_reg.out", sep=""))
+    tmp[,rep:=i]
+    tmp <- cbind(snp.meta, tmp)
+    return(tmp)
+}
+
+# Change column names
+setnames(bf.Mtross.mean, "BF(dB)", "bf_db")
+
+# ================================================================================== #
+
+# Average BF across replicate runs
+bf.Mtross.mean.sum <- bf.Mtross.mean %>% group_by(chr, pos, allele1, allele2, MRK) %>% 
+    reframe(bf_db.mean = mean(bf_db), bf_db.median = median(bf_db),
+            bf_db.var=var(bf_db), eBPis.mean=mean(eBPis), eBPis.median=median(eBPis), eBPis.var=var(eBPis))
+
+# Save
+save(bf.Mtross.mean.sum, file="data/processed/baypass/biotic/bf.Mtross.mean.sum.Rdata")
+
+# ================================================================================== #
+
+# Graph BF with 0.001 POD threshold
+pdf("output/figures/baypass/baypass_BF_Mtross_mean_repmeans.pdf", width = 12, height = 8)
+ggplot(bf.Mtross.mean.sum, aes(y=bf_db.mean, x=chr)) + 
+  labs(x = "Position", y = "BF (in dB)") +
+  geom_point(alpha=0.6) + 
+  geom_hline(yintercept=bf.POD.thr$bf_db.mean[which(bf.POD.thr$thr==0.999)], col="red") +
+  theme_classic(base_size = 20) +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        axis.text.y = element_text(size = 12))
+dev.off()
+
+# Graph BF with 0.001 POD threshold - only BF > 0
+pdf("output/figures/baypass/baypass_BF_Mtross_mean_repmeans_posBF.pdf", width = 12, height = 8)
+ggplot(bf.Mtross.mean.sum[which(bf.Mtross.mean.sum$bf_db.mean>0),], aes(y=bf_db.mean, x=chr)) + 
+  labs(x = "Position", y = "BF (in dB)") +
+  geom_point(alpha=0.6) + 
+  geom_hline(yintercept=bf.POD.thr$bf_db.mean[which(bf.POD.thr$thr==0.999)], col="red") +
+  theme_classic(base_size = 20) +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        axis.text.y = element_text(size = 12))
+dev.off()
+
+# ================================================================================== #
+
+# Identify patterns in bayes factors
+
+# Mtross mean - 2,417 SNPs with BF > threshold
+bf.Mtross.mean.sum.outliers <- bf.Mtross.mean.sum[which(bf.Mtross.mean.sum$bf_db.mean > bf.POD.thr$bf_db.mean[which(bf.POD.thr$thr==0.999)]),]
+bf.Mtross.mean.sum.outliers <- bf.Mtross.mean.sum.outliers %>% mutate(SNP_id = paste(chr, pos, sep = "_"))
+write.csv(bf.Mtross.mean.sum.outliers, "data/processed/baypass/bf.Mtross.mean.sum.outliers.csv", row.names = F, quote = F)
