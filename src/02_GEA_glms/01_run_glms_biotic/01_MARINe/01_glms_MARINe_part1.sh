5bd92f6586180809a17101467c19f9dc1d331e8b
Emily Longman <Emily.Longman@uvm.edu>
2026-01-21 17:55:20 -0500

    Update scripts

--- src/02_GEA_glms/01_run_glms_biotic/01_MARINe/01_glms_MARINe_part1.sh
+++ src/02_GEA_glms/01_run_glms_biotic/01_MARINe/01_glms_MARINe_part1.sh
@@ -23,7 +23,7 @@
 #SBATCH --cpus-per-task=5
 
 # Submit job array
-#SBATCH --array=401-500 #1-400%75 #500
+#SBATCH --array=17 #1-500%75
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
@@ -98,8 +98,8 @@ fi
 # Run R script
 
 #Rscript $SCRIPT_FOLDER/01_glms_MARINe_real.R "${SLURM_ARRAY_TASK_ID}"
-#Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
-Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm26:50.R "${SLURM_ARRAY_TASK_ID}"
+Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
+#Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm26:50.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm51:75.R "${SLURM_ARRAY_TASK_ID}"
 #Rscript $SCRIPT_FOLDER/01_glms_MARINe_perm76:100.R "${SLURM_ARRAY_TASK_ID}"
 
