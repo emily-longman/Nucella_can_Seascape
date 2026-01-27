4fc0719bcf0d03f2fef26fcba2b5bd121a829ca9
Emily Longman <Emily.Longman@uvm.edu>
2026-01-27 15:45:18 -0500

    Update 08_graph_annotated_Mtross_mean.R

--- src/03_baypass/08_graph_annotated_Mtross_mean.R
+++ src/03_baypass/08_graph_annotated_Mtross_mean.R
@@ -17,7 +17,7 @@ setwd(root_path)
 # ================================================================================== #
 
 # Load packages
-#install.packages(c('data.table', 'tidyverse', 'foreach', 'dplyr', 'ggplot2', 'RColorBrewer', 'poolfstat))
+#install.packages(c('data.table', 'tidyverse', 'foreach', 'dplyr', 'ggplot2', 'RColorBrewer', 'poolfstat', 'reshape2', 'maps', mapdata'))
 library(data.table)
 library(tidyverse)
 library(foreach)
@@ -25,6 +25,9 @@ library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
 library(poolfstat)
+library(reshape2)
+library(maps)
+library(mapdata)
 
 # ================================================================================== #
 
@@ -77,17 +80,21 @@ dev.off()
 # Load pooldata
 load("data/raw/pooldata/pooldata.RData")
 
+# Metadata
+meta <- fread("guide_files/Populations_metadata.csv")
+
+###
+
 # Get the snp.info matrix
 snp.info <- pooldata@snp.info
 snp.info <- snp.info %>% mutate(SNP_id = paste(Chromosome, Position, sep = "_"))
 # Add column for g26813
 snp.info <- snp.info %>% mutate(g26813 = SNP_id %in% g26813$SNP_id)
 
-
 # Subset the pooldata object using the selected SNP indices
 pooldata.sub <- pooldata.subset(pooldata, snp.index = which(snp.info$g26813 == TRUE))
 
-
+###
 
 # Extract read count and coverage data for SNPs
 snp.info.sub <- pooldata.sub@snp.info
@@ -100,42 +107,49 @@ afs <- afs %>% as.data.frame()
 
 # Rename columns (19 sites and SNP_ids)
 names(afs) = c(pooldata.sub@poolnames)
-rownames(afs) = rownames(snp.info.sub)
+afs$SNP_id = rownames(snp.info.sub)
+
+# Change to long format
+afs.melt <- melt(afs, id = "SNP_id", variable.name = "Site", value.name = "AF")
+
+# Join with metadata 
+afs.melt <- left_join(afs.melt, meta, by = "Site")
+
+afs.melt$Site <- factor(afs.melt$Site, levels=c("FC", "SLR", "SH", "ARA", "CBL", "PSG", "STC", "KH", "VD", "FR", "BMR", "PGP", "PL", "SBR", "PSN", "PB", "HZD", "OCT", "STR"))
+
+
+###
 
 # Graph allele frequencies
+
+# Get state data
+states <- map_data("state")
+# Subset data for only California and Oregon
+west_coast <- subset(states, region %in% c("california", "oregon"))
 
 # Color palette
 nb.cols <- 19
 mycolors <- rev(colorRampPalette(brewer.pal(11, "RdBu"))(nb.cols))
 
 # AF versus latitude
-pdf("output/figures/baypass/Outlier_AF_lat.pdf", width = 18, height = 4)
-ggplot(data = afs.id.mapped.targets, aes(x = Lat, y = AF)) +
-  geom_point(aes(fill = Site), size = 5, shape = 21) + 
+pdf("output/figures/baypass/Outlier_AF_lat.pdf", width = 16, height = 12)
+ggplot(data = afs.melt, aes(x = Lat, y = AF)) +
+  geom_point(aes(fill = Site), size = 3, shape = 21) + 
   geom_vline(xintercept=36.8007, linetype="solid", color="black") +
   ylab("Allele Frequency") + xlab("Latitude") +
   scale_fill_manual(values = mycolors) +
-  facet_grid(~pos) + theme_bw(base_size = 22) + theme(legend.position="none")
+  facet_wrap(~SNP_id) + theme_bw(base_size = 14) + theme(legend.position="none")
 dev.off()
 
 
 # Graph map
-pdf("output/figures/morphology/Allele_freq_maps.pdf", width = 8, height = 3)
-ggplot(data = world) +
-  geom_sf(fill= "grey70") +
-  coord_sf(xlim = c(-127, -115), ylim = c(32, 47), expand = FALSE) + 
-  theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", linewidth = 0.2), 
-        panel.background = element_rect(fill = "white")) +
-  geom_jitter(data = afs.id.mapped.targets,
-              color = "black",
-              aes(
-                x=Long,
-                y=Lat,
-                fill = AF,
-                shape = cluster
-              ), alpha = 0.9, size = 2.5) + 
-  scale_fill_gradient2(low = "steelblue", ,high = "firebrick",
-                       midpoint = 0.5) +
-  scale_shape_manual(values = 21:23) +
-  facet_grid(~snp_id) + theme_bw()
+pdf("output/figures/baypass/Outlier_4922150_AF_map.pdf", width = 8, height = 8)
+ggplot(data = west_coast) + 
+  geom_polygon(aes(x = long, y = lat, group = group), fill = "white", color = "black") + 
+  geom_point(data = afs.melt[which(afs.melt$SNP_id == "4922150"),], aes(x = Long, y = Lat, fill = AF), shape = 21, size = 5) + 
+  scale_fill_gradient(low = "firebrick", high = "black") + 
+             coord_fixed(1.3) +
+  xlim(c(-128, -114)) +
+  xlab("Longitude") + ylab("Latitude") + theme_classic(base_size = 12) + ggtitle("AF 4922150")
 dev.off()
+
