c1fb58a73b6454caa47b8f7e1be4fdb566ab26ce
Emily Longman <Emily.Longman@uvm.edu>
2026-02-03 08:47:44 -0500

    Update 05_add_RG.sh

--- src/repadapt/05_add_RG.sh
+++ src/repadapt/05_add_RG.sh
@@ -16,9 +16,9 @@ WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Seascape
 
 # Make folders
 cd $WORKING_FOLDER/data/processed/repadapt
-if [ -d "remove_RG" ]
-then echo "Working remove_RG folder exist"; echo "Let's move on."; date
-else echo "Working remove_RG folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/data/processed/repadapt/remove_RG; date
+if [ -d "add_RG" ]
+then echo "Working add_RG folder exist"; echo "Let's move on."; date
+else echo "Working add_RG folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/data/processed/repadapt/add_RG; date
 fi
 
 # Use apptainer to use the correct versions of programs
@@ -35,7 +35,7 @@ JAVAMEM=20G
 # Add read groups, we start with our deduplicated bam files and we get a deduplicated bam with read groups assigned per sample/library
 apptainer run $repadapt_picard picard -Xmx20G AddOrReplaceReadGroups \
 I=$WORKING_FOLDER/data/processed/repadapt/remove_dup/${i}_sorted_dedup.bam \
-O=$WORKING_FOLDER/data/processed/repadapt/remove_dup/${i}_sorted_dedup_RG.bam \
+O=$WORKING_FOLDER/data/processed/repadapt/add_RG/${i}_sorted_dedup_RG.bam \
 RGID=${i} RGLB=${i}\_LB RGPL=ILLUMINA RGPU=unit1 RGSM=${i}
 
 #--------------------------------------------------------------------------------
