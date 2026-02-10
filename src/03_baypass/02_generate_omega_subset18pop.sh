77dfded876d02a37382dff90ff6854c30a4ecd22
Emily Longman <Emily.Longman@uvm.edu>
2026-02-10 16:11:43 +0100

    Update 02_generate_omega_subset18pop.sh

--- src/03_baypass/02_generate_omega_subset18pop.sh
+++ src/03_baypass/02_generate_omega_subset18pop.sh
@@ -14,7 +14,7 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=3-00:00:00 
+#SBATCH --time=5-00:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=30G 
