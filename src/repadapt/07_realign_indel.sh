4ab465290b67beda36e32459d91efa96005f272f
Emily Longman <Emily.Longman@uvm.edu>
2026-03-12 20:57:14 -0400

    Update 07_realign_indel.sh

--- src/repadapt/07_realign_indel.sh
+++ src/repadapt/07_realign_indel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --partition=general #week
+#SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
-#SBATCH --time=16:00:00
-#SBATCH --cpus-per-task=3 #5
+#SBATCH --time=18:00:00
+#SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=300G
-#SBATCH --array=14-18 #1-18 #19
+#SBATCH --array=1-19
 
 #--------------------------------------------------------------------------------
 # My additions:
