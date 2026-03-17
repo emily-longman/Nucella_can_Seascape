4bdd19eec3259523923f783838d60e25708bf9c7
Emily Longman <Emily.Longman@uvm.edu>
2026-03-17 10:37:20 -0400

    Update 08_cnv_2.sh

--- src/repadapt/08_cnv_2.sh
+++ src/repadapt/08_cnv_2.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=emily.longman@uvm.edu 
 #SBATCH --time=05:00:00
-#SBATCH --mem-per-cpu=5G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --array=1-19
 
 #--------------------------------------------------------------------------------
@@ -40,14 +40,14 @@ echo \n">>> Computing depth of each gene for ${i} <<<"\n
 awk '{print $1"\t"$2"\t"$2"\t"$3}' $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned.depth | apptainer run $repadapt_bedtools bedtools map -a $WORKING_FOLDER/data/processed/repadapt/cnv/genes.bed -b stdin -c 4 -o mean -null 0 -g $WORKING_FOLDER/data/processed/repadapt/cnv/genome.bed | awk -F "\t" '{print $1":"$2"-"$3"\t"$4}' | sort -k1,1 > $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-genes.tsv
 
 # sort gene depth results based on input bed file
-join -a 1 -e 0 -o '1.1 2.2' -t $'\t' genes.list $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-genes.tsv > $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-genes.sorted.tsv
+join -a 1 -e 0 -o '1.1 2.2' -t $'\t' $WORKING_FOLDER/data/processed/repadapt/cnv/genes.list $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-genes.tsv > $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-genes.sorted.tsv
 
 # window depth analysis
 echo \n">>> Computing depth of each window for ${i} <<<"\n
 awk '{print $1"\t"$2"\t"$2"\t"$3}' $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned.depth | apptainer run $repadapt_bedtools bedtools map -a $WORKING_FOLDER/data/processed/repadapt/cnv/windows.bed -b stdin -c 4 -o mean -null 0 -g $WORKING_FOLDER/data/processed/repadapt/cnv/genome.bed | awk -F "\t" '{print $1":"$2"-"$3"\t"$4}' | sort -k1,1  > $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-windows.tsv
 
 # sort window depth results based on input bed file
-join -a 1 -e 0 -o '1.1 2.2' -t $'\t' $WORKING_FOLDER/data/processed/repadapt/realign_indel/windows.list $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-windows.tsv > $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-windows.sorted.tsv
+join -a 1 -e 0 -o '1.1 2.2' -t $'\t' $WORKING_FOLDER/data/processed/repadapt/cnv/windows.list $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-windows.tsv > $WORKING_FOLDER/data/processed/repadapt/realign_indel/${i}_sorted_dedup_RG_lanes_merged_realigned-windows.sorted.tsv
 
 # overall genome depth
 echo \n">>> Computing depth of whole genome for ${i} <<<"\n
