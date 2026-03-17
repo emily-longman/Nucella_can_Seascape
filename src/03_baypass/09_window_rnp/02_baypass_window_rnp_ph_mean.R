8438cbc6005684eb9edeae765abce58766922570
Emily Longman <Emily.Longman@uvm.edu>
2026-03-17 17:56:20 -0400

    Create ph mean window analysis

---
+++ src/03_baypass/09_window_rnp/02_baypass_window_rnp_ph_mean.R
@@ -0,0 +1,106 @@
+# Create windows 
+
+# Clear memory
+rm(list=ls())
+
+# ================================================================================== #
+
+# Set path as main Github repo
+# Install and load package
+#install.packages(c('rprojroot'))
+library(rprojroot)
+# Specify root path
+root_path <- find_root_file(criterion = has_file("README.md"))
+# Set working directory as path from root
+setwd(root_path)
+
+# ================================================================================== #
+
+# Load packages
+#install.packages(c('data.table', 'tidyverse', 'foreach', 'dplyr', 'doMC'))
+library(data.table)
+library(tidyverse)
+library(foreach)
+library(dplyr)
+
+# ================================================================================== #
+
+# Specify arguments
+args = commandArgs(trailingOnly=TRUE)
+win_group = as.numeric(args[1])
+
+# ================================================================================== #
+
+# State variable name
+message(paste("Window group:", win_group))
+# Load windows
+load("data/processed/baypass/window_summary/windows.RData")
+# Extract windows of interest based on win_group
+wins_group_i <- wins_guide_file_array %>% filter(.groups == win_group)
+
+# Load mean bf data from 5 baypass runs
+load("data/processed/baypass/abiotic/bf.ph.mean.sum.Rdata")
+
+# ================================================================================== #
+
+# Rank-normalize bf (note: high bf should be associated with a low rank)
+bf.ph.mean.sum$rank <- rank(-bf.ph.mean.sum$bf_db.mean)
+Lp <- length(bf.ph.mean.sum$bf_db.mean)
+bf.ph.mean.sum$rnp <- bf.ph.mean.sum$rank/Lp
+
+# ================================================================================== #
+
+# Window summarization
+
+# Start window summarization process for windows within window group
+win.out <- foreach(window.w=1:dim(wins_group_i)[1], .combine = "rbind", .errorhandling = "remove")%do%{
+
+        # State window
+        message(paste0("Window ", window.w, " / ", dim(wins_group_i)[1], " for window group"))
+        message(paste0("i.e., Window #: ", wins_group_i$i[window.w]))
+  
+        # Filter glm data for a given window
+        win_tmp <- bf.ph.mean.sum %>%
+        filter(chr == wins_group_i$chr[window.w]) %>%
+        filter(pos >= wins_group_i$start[window.w] & pos <= wins_group_i$end[window.w])
+    
+        # P-values
+        pr.i.0.05 <- c(0.05)
+        pr.i.0.01 <- c(0.01)
+    
+        # Summarize for a given window
+        win_tmp %>% 
+            filter(!is.na(rnp)) %>%
+            summarise(
+              chr = unique(chr),
+              pos_mean = mean(pos),
+              pos_min = min(pos),
+              pos_max = max(pos),
+              bf_mean = mean(bf_db.mean),
+              bf_min = min(bf_db.mean),
+              bf_max = max(bf_db.mean),
+              win = wins_group_i$i[window.w],
+              rnp.pr.0.05 = c(mean(rnp <= pr.i.0.05)),
+              rnp.pr.0.01 = c(mean(rnp <= pr.i.0.01)),
+              rnp.binom.p.0.05 = c(binom.test(sum(rnp <= pr.i.0.05), length(rnp), pr.i.0.05)$p.value),
+              rnp.binom.p.0.01 = c(binom.test(sum(rnp <= pr.i.0.01), length(rnp), pr.i.0.01)$p.value),
+              sum.rnp.0.05 = sum(rnp <= pr.i.0.05),
+              sum.rnp.0.01 = sum(rnp <= pr.i.0.01),
+              max.rnp = max(rnp),
+              min.rnp = min(rnp),
+              nSNPs = n()
+            )
+}
+
+# ================================================================================== #
+
+# Generate folders and save output
+
+# Folder name for window group i
+folder_name <- paste("data/processed/baypass/window_summary/window_chunk_analysis_ph_mean")
+
+# Save file for chunk w
+file_name <- paste0("window_chunks_", win_group)
+save(win.out, file = paste0(folder_name, "/", file_name, ".Rdata") )
+
+message("done")
---
+++ src/03_baypass/09_window_rnp/03_graph_win_Mtross_mean.R
@@ -0,0 +1,84 @@
+# Create windows
+
+# Clear memory
+rm(list=ls())
+
+# ================================================================================== #
+
+# Set path as main Github repo
+# Install and load package
+#install.packages(c('rprojroot'))
+library(rprojroot)
+# Specify root path
+root_path <- find_root_file(criterion = has_file("README.md"))
+# Set working directory as path from root
+setwd(root_path)
+
+# ================================================================================== #
+
+# Load packages
+#install.packages(c('data.table', 'tidyverse', 'foreach', 'dplyr', 'doMC'))
+library(data.table)
+library(tidyverse)
+library(foreach)
+library(dplyr)
+
+# ================================================================================== #
+
+# Generate output directories
+
+# Figure directory
+out_fig_dir <- paste("output/figures/baypass/window_summary")
+if (!dir.exists(out_fig_dir)) {dir.create(out_fig_dir)}
+
+# ================================================================================== #
+
+# Load and merge data
+
+# Create list of file names
+path <- paste("data/processed/baypass/window_summary/window_chunk_analysis_Mtross_mean/")
+file_names = as.list(dir(path = path, pattern = "window_chunks_*"))
+file_names_v = as.vector(unlist(lapply(file_names, function(x) paste0(paste("data/processed/baypass/window_summary/window_chunk_analysis_Mtross_mean/"), x))))
+
+# Check number of files
+length(file_names_v)
+
+# Read all the files and add a column with the chunk
+win.out =  
+foreach(i=file_names_v, .combine="rbind", .errorhandling = "remove")%do%{  
+    # State which file loading
+    message(i)
+    # Load file
+    o = get(load(i))
+}
+
+# Check structure
+str(win.out)
+
+# ================================================================================== #
+
+# Save merged data
+save(win.out, file = "data/processed/baypass/window_summary/window_analysis_Mtross_mean.RData")
+
+# ================================================================================== #
+
+# Create unique Chromosome number
+win.out.chr.unique <- unique(win.out$chr)
+win.out$chr.unique <- as.numeric(factor(win.out$chr, levels = win.out.chr.unique))
+
+# Graph rnp p
+
+# Graph rnp geompoint
+pdf("output/figures/baypass/window_summary/baypass_window_Mtross_mean_rnp_0.05_geompoint.pdf", width = 12, height = 6)
+ggplot(win.out, aes(y=-log10(rnp.binom.p.0.05), x=chr.unique)) + 
+  geom_point(alpha=0.8, size=1.3) + 
+  theme_bw(base_size=26) + theme(legend.position = "none")
+dev.off()
+
+# Graph rnp geomline
+pdf("output/figures/baypass/window_summary/baypass_window_Mtross_mean_rnp_0.05_geomline.pdf", width = 12, height = 6)
+ggplot(win.out, aes(y=-log10(rnp.binom.p.0.05), x=chr.unique)) + 
+  geom_line( ) + 
+  theme_bw(base_size=26) + theme(legend.position = "none")
+dev.off()
+
