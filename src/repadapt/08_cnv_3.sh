4ef3bc90989c3fbb80946b11ce393c7190012cb2
Emily Longman <Emily.Longman@uvm.edu>
2026-03-17 13:08:46 -0400

    Update 08_cnv_3.sh

--- src/repadapt/08_cnv_3.sh
+++ src/repadapt/08_cnv_3.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=emily.longman@uvm.edu 
 #SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=5G
 
 #--------------------------------------------------------------------------------
 # My additions/modifications:
