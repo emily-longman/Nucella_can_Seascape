e693d2707fcd748ef7d2e3a956b863b8be416e3f
Emily Longman <Emily.Longman@uvm.edu>
2026-03-31 11:03:50 -0400

    Update 02_baypass_window_rnp_ph_mean.sh

--- src/04_baypass/09_window_rnp_50kb/02_baypass_window_rnp_ph_mean.sh
+++ src/04_baypass/09_window_rnp_50kb/02_baypass_window_rnp_ph_mean.sh
@@ -20,7 +20,7 @@
 #SBATCH --mem=2G 
 
 # Submit job array
-#SBATCH --array=1 #1-495%100
+#SBATCH --array=1-495%100
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
