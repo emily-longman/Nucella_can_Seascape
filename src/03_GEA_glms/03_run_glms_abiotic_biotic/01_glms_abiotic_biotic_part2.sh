18550f5d6d35231d51a74ed825551ae31566aa7a
Emily Longman <Emily.Longman@uvm.edu>
2026-06-30 13:45:39 -0400

    Update 01_glms_abiotic_biotic_part2.sh

--- src/03_GEA_glms/03_run_glms_abiotic_biotic/01_glms_abiotic_biotic_part2.sh
+++ src/03_GEA_glms/03_run_glms_abiotic_biotic/01_glms_abiotic_biotic_part2.sh
@@ -23,7 +23,7 @@
 #SBATCH --cpus-per-task=5
 
 # Submit job array
-#SBATCH --array=501 #501-996%100
+#SBATCH --array=501-996%100
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
@@ -97,8 +97,8 @@ fi
 
 # Run R script
 
-#Rscript $SCRIPT_FOLDER/01_glms_abiotic_biotic_real.R "${SLURM_ARRAY_TASK_ID}"
-Rscript $SCRIPT_FOLDER/01_glms_abiotic_biotic_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
+Rscript $SCRIPT_FOLDER/01_glms_abiotic_biotic_real.R "${SLURM_ARRAY_TASK_ID}"
+#Rscript $SCRIPT_FOLDER/01_glms_abiotic_biotic_perm1:25.R "${SLURM_ARRAY_TASK_ID}"
 
 #--------------------------------------------------------------------------------
 
