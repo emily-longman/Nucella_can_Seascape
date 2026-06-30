138f4a38db2ca7cf26f24162bc5100efeeca02ff
Emily Longman <Emily.Longman@uvm.edu>
2026-06-30 12:44:05 -0400

    Update time for scripts

--- src/03_GEA_glms/03_run_glms_abiotic_biotic/01_glms_abiotic_biotic_part1.sh
+++ src/03_GEA_glms/03_run_glms_abiotic_biotic/01_glms_abiotic_biotic_part1.sh
@@ -14,7 +14,7 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=5:00:00 #25:00:00 for perm
+#SBATCH --time=2:00:00 #25:00:00 for perm
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
 #SBATCH --mem=30G 
