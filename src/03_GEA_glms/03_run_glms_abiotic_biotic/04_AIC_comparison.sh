bdc08de11ba62a004d984234e8f74dbe5b39c3ce
Emily Longman <Emily.Longman@uvm.edu>
2026-07-02 12:45:01 -0400

    Update 04_AIC_comparison.sh

--- src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.sh
+++ src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.sh
@@ -20,7 +20,7 @@
 #SBATCH --mem=200G 
 
 # Submit job array
-#SBATCH --array=1-920%100
+#SBATCH --array=1-920%500
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%j.out
