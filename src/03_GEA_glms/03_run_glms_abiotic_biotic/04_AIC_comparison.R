03e54758ba5cd6bf97ce16f201576140ceb159bb
Emily Longman <Emily.Longman@uvm.edu>
2026-07-02 12:44:34 -0400

    Create guide file for AIC comparision

--- src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.R
+++ src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.R
@@ -25,6 +25,12 @@ library(foreach)
 
 # ================================================================================== #
 
+# Specify arguments
+args = commandArgs(trailingOnly=TRUE)
+w = as.numeric(args[1]) # Chunk
+
+# ================================================================================== #
+
 # Generate output directories
 out_dir <- paste("data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic")
 if (!dir.exists(out_dir)) {dir.create(out_dir)}
@@ -40,41 +46,55 @@ glm.real <- glm.model.collated.real
 load("data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic/glm.model.collated.perm.Rdata")
 glm.perm <- perm
 
+# Load snp info with groups
+load("data/processed/GEA/glms/snp.guide.file.Rdata")
+
+# ================================================================================== #
+
+# Subset real and perm data based on chunk
+
+# Subset snp.guide. file
+snp.chunk <- snp.guide.file %>% filter(.groups == w)
+# Subset real data
+glm.real.chunk <- glm.real %>% filter(SNP_id %in% snp.chunk$SNP_id)
+# Subset perm data
+glm.perm.chunk <- glm.perm %>% filter(SNP_id %in% snp.chunk$SNP_id)
+
 # ================================================================================== #
 
 # Identify which model is best for the real data
 
 # Identify which column is the minimum
 #glm.real$minAIC <- names(glm.real[, 8:13])[apply(glm.real[, 8:13], MARGIN = 1, FUN = which.min)]
-glm.real$minAIC_value <- do.call(pmin, c(glm.real[, 8:13], na.rm = TRUE))
+glm.real.chunk$minAIC_value <- do.call(pmin, c(glm.real.chunk[, 8:13], na.rm = TRUE))
 
 # Calc delta AIC between AIC_dem_both_int and model with min AIC
-glm.real <- glm.real %>% mutate(deltaAIC = AIC_dem_both_int-minAIC_value)
+glm.real.chunk <- glm.real.chunk %>% mutate(deltaAIC = AIC_dem_both_int-minAIC_value)
 
 # ================================================================================== #
 
 # Identify which model is best for the perm data
 
 # Identify which column is the minimum
-glm.perm$minAIC_value <- do.call(pmin, c(glm.perm[, 8:13], na.rm = TRUE))
+glm.perm.chunk$minAIC_value <- do.call(pmin, c(glm.perm.chunk[, 8:13], na.rm = TRUE))
 
 # Calc delta AIC between AIC_dem_both_int and model with min AIC
-glm.perm <- glm.perm %>% mutate(deltaAIC = AIC_dem_both_int-minAIC_value)
+glm.perm.chunk <- glm.perm.chunk %>% mutate(deltaAIC = AIC_dem_both_int-minAIC_value)
 
 # ================================================================================== #
 
 # Use permutations to calc p-value
 
 # Identify which SNPs in the real data have the model with the interaction as the best fit
-glm.real.int <- glm.real[which(glm.real$deltaAIC == 0),]
+glm.real.chunk.int <- glm.real.chunk[which(glm.real.chunk$deltaAIC == 0),]
 # Identify which SNPs in the real data dont have the model with the interaction as the best fit
-glm.real.no.int <- glm.real[which(glm.real$deltaAIC != 0),]
+glm.real.chunk.no.int <- glm.real.chunk[which(glm.real.chunk$deltaAIC != 0),]
 
 # Use permutations to calc p-val for SNPs where model with interaction is best fit 
-o.int = foreach(i=1:dim(glm.real.int)[1], .combine = "rbind")%do%{
+o.int = foreach(i=1:dim(glm.real.chunk.int)[1], .combine = "rbind")%do%{
     
     # Extract perm data for focal SNP
-    perm.tmp <- glm.perm[which(glm.perm$SNP_id == glm.real.int$SNP_id[i]),]
+    perm.tmp <- glm.perm.chunk[which(glm.perm.chunk$SNP_id == glm.real.chunk.int$SNP_id[i]),]
 
     # Make data table and calculate p-val based on permutations
     data.frame(
@@ -85,15 +105,24 @@ o.int = foreach(i=1:dim(glm.real.int)[1], .combine = "rbind")%do%{
 }
 
 # For all models where the interaction is not the best fit, set p-val equal to 1
-o.no.int <- no.int[,1:3] %>% mutate(p_val = 1)
+o.no.int <- glm.real.chunk.no.int[,1:3] %>% mutate(p_val = 1)
 
 # Join data
 o <- rbind(o.int, o.no.int)
 
 # ================================================================================== #
 
-# Save output
-save(o, file = paste("data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic/glm.output.Rdata"))
+# Generate folders and save output
+
+# Folder name
+folder_name <- paste("data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic/output_chunks")
+if (!dir.exists(folder_name)) {dir.create(folder_name)}
+
+# Save file for chunk w
+file_name <- paste("GLM_output_chunk_", w, sep = "")
+save(o, file = paste(folder_name, "/" , file_name, ".Rdata", sep = "") )
+
+message("done")
 
 # ================================================================================== #
 # ================================================================================== #
--- src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.sh
+++ src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.sh
@@ -14,10 +14,13 @@
 #SBATCH --nodes=1 
 
 # Reserve walltime -- hh:mm:ss --30 hrs max
-#SBATCH --time=30:00:00 
+#SBATCH --time=10:00:00 
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=900G 
+#SBATCH --mem=200G 
+
+# Submit job array
+#SBATCH --array=1-920%100
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%j.out
@@ -43,8 +46,31 @@ WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Seascape
 
 #--------------------------------------------------------------------------------
 
+# Determine partition to process 
+
+# Set chunk (w) equal to slurm array
+w=${SLURM_ARRAY_TASK_ID}
+
+# Echo chunk
+echo ${w}
+
+#--------------------------------------------------------------------------------
+
+# Generate Folders and files
+
+# Move to working directory
+cd $WORKING_FOLDER/data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+if [ -d "output_chunks" ]
+then echo "Working output_chunks folder exist"; echo "Let's move on."; date
+else echo "Working output_chunks folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic/output_chunks; date
+fi
+
+#--------------------------------------------------------------------------------
+
 # Run R script
-Rscript --vanilla $WORKING_FOLDER/src/03_GEA_glms/03_run_glms_abiotic_biotic/03_AIC_comparison.R
+Rscript --vanilla $WORKING_FOLDER/src/03_GEA_glms/03_run_glms_abiotic_biotic/04_AIC_comparison.R "${w}"
 
 #--------------------------------------------------------------------------------
 
