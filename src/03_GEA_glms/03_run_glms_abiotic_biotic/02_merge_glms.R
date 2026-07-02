f8d1a8f47de3aba09912d36cd5a09f167f92ea0e
Emily Longman <Emily.Longman@uvm.edu>
2026-07-02 11:10:59 -0400

    Update 02_merge_glms.R

--- src/03_GEA_glms/03_run_glms_abiotic_biotic/02_merge_glms.R
+++ src/03_GEA_glms/03_run_glms_abiotic_biotic/02_merge_glms.R
@@ -114,6 +114,3 @@ perm <- rbind(glm.model.collated.perm1.50, glm.model.collated.perm51.100)
 
 # Save merged data
 save(perm, file = paste("data/processed/GEA/glms/glms_chunk_analysis_abiotic_biotic/glm.model.collated.perm.Rdata"))
-
-
-
