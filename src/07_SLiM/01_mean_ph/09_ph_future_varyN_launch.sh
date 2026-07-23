70e58ad73812ec510220954737ccadc7d2fa6b5c
Emily Longman <Emily.Longman@uvm.edu>
2026-07-23 09:11:42 -0400

    Create vary N ph future slim

---
+++ src/07_SLiM/01_mean_ph/09_ph_future_varyN_launch.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+# In the command line, run the following command: sbatch path/to/this/file.sh
+
+# Request cluster resources ----------------------------------------------------
+
+# Name this job
+#SBATCH --job-name=ph_future_vary_N
+
+# Specify partition
+#SBATCH --partition=general
+
+# Request nodes
+#SBATCH --nodes=1 
+
+# Reserve walltime -- hh:mm:ss --30 hrs max
+#SBATCH --time=02:00:00
+
+# Request memory for the entire job -- you can request --mem OR --mem-per-cpu
+#SBATCH --mem=10G
+
+# Name output of this job using %x=job-name and %j=job-id
+#SBATCH --output=./slurmOutput/%x.%j.out
+
+# Receive emails when job begins and ends or fails
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=emily.longman@uvm.edu 
+
+#--------------------------------------------------------------------------------
+
+# This script will run the accompanying slim script. 
+
+# Load modules 
+module load slim/5.0
+
+#--------------------------------------------------------------------------------
+
+# Define important file locations
+
+# WORKING_FOLDER is the core folder where this pipeline is being run.
+WORKING_FOLDER=/gpfs2/scratch/elongman/Nucella_can_Seascape
+
+#--------------------------------------------------------------------------------
+
+# Determine parameters
+
+# Establish the array
+arr=("2500" "3750" "5000" "10000")
+N="${arr[$SLURM_ARRAY_TASK_ID]}"
+echo "N:" ${N}
+
+# Set root
+ROOT=$WORKING_FOLDER/data/processed/SLiM/ph_future/ph_results
+
+#--------------------------------------------------------------------------------
+
+# Generate Folders and files
+
+# Change directory
+cd $WORKING_FOLDER/data/processed/SLiM/ph_future
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+if [ -d "ph_vary_N" ]
+then echo "Working ph_vary_N folder exist"; echo "Let's move on."; date
+else echo "Working ph_vary_N folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/data/processed/SLiM/ph_future/ph_vary_N; date
+fi
+
+# This part of the script will check and generate, if necessary, all of the output folders used in the script
+if [ -d "N_${N}" ]
+then echo "Working N_${N} folder exist"; echo "Let's move on."; date
+else echo "Working N_${N} folder doesnt exist. Let's fix that."; mkdir $WORKING_FOLDER/data/processed/SLiM/ph_future/ph_vary_N/N_${N}; date
+fi
+
+#--------------------------------------------------------------------------------
+
+# Change directory
+cd $WORKING_FOLDER/data/processed/SLiM/ph_future/ph_results/ph_vary_N/N_${N}
+
+# Loop through iterations
+for i in {1..100}
+do
+
+# Run slim script
+slim \
+    -d "repId=${i}" \
+    -d "N=${N}" \
+	-d "root='${ROOT}'" \
+     $WORKING_FOLDER/src/07_SLiM/01_mean_ph/09_ph_future_varyN.slim
+
+done
+
+#--------------------------------------------------------------------------------
+
+# Say done
+echo "done"
