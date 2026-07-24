b852220f7a1406f2cb585379f56265298161d978
Emily Longman <Emily.Longman@uvm.edu>
2026-07-24 06:43:37 -0400

    Update 02_Mcali_slim_launch.sh

--- src/07_SLiM/02_Mcali/02_Mcali_slim_launch.sh
+++ src/07_SLiM/02_Mcali/02_Mcali_slim_launch.sh
@@ -20,7 +20,7 @@
 #SBATCH --mem=10G 
 
 # Submit job array
-#SBATCH --array=1-936
+#SBATCH --array=1-500 #1-936
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
