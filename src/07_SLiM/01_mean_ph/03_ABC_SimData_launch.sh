9c6a4a77fb3ee2b1da71ce152bba866287055ac2
Emily Longman <Emily.Longman@uvm.edu>
2026-07-29 16:47:18 -0400

    Update 03_ABC_SimData_launch.sh

--- src/07_SLiM/01_mean_ph/03_ABC_SimData_launch.sh
+++ src/07_SLiM/01_mean_ph/03_ABC_SimData_launch.sh
@@ -17,7 +17,7 @@
 #SBATCH --time=30:00:00
 
 # Request memory for the entire job -- you can request --mem OR --mem-per-cpu
-#SBATCH --mem=500G 
+#SBATCH --mem=600G 
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%j.out
