3b5540f9c833c93da12f84954c8a1c67341c52b8
Emily Longman <Emily.Longman@uvm.edu>
2026-08-04 15:08:42 -0400

    Update 09_ph_future_vary_m_launch.sh

--- src/07_SLiM/01_mean_ph/09_ph_future_vary_m_launch.sh
+++ src/07_SLiM/01_mean_ph/09_ph_future_vary_m_launch.sh
@@ -20,7 +20,7 @@
 #SBATCH --mem=2G
 
 # Submit job array
-#SBATCH --array=1-3
+#SBATCH --array=4-5 #1-3
 
 # Name output of this job using %x=job-name and %j=job-id
 #SBATCH --output=./slurmOutput/%x.%A_%a.out
