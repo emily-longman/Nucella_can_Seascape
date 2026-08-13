b8aeeffbe9126029eeed4de3c354c033e1672f10
Emily Longman <Emily.Longman@uvm.edu>
2026-08-13 10:29:59 -0400

    Update 02_Mcali_slim_launch.sh

--- src/07_SLiM/02_Mcali/02_Mcali_slim_launch.sh
+++ src/07_SLiM/02_Mcali/02_Mcali_slim_launch.sh
@@ -5,7 +5,7 @@
 # Request cluster resources ----------------------------------------------------
 
 # Name this job
-#SBATCH --job-name=Mcali_slim_v8_pt2
+#SBATCH --job-name=Mcali_slim_v8_pt3
 
 # Specify partition
 #SBATCH --partition=general
@@ -96,8 +96,8 @@ cd $WORKING_FOLDER/data/processed/SLiM/Mcali_results_v8
 
 # Loop through iterations
 #for i in {1..25}
-for i in {26..50}
-#for i in {51..75}
+#for i in {26..50}
+for i in {51..75}
 #for i in {76..100}
 do
 
